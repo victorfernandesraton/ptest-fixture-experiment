72d90125153c7a378774e5b481cc23a8c3ab5885
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-05-04 19:13:09 -0300

    wip: better tests name

--- tests/delivery/test_estimate_delivery_service.py
+++ tests/delivery/test_estimate_delivery_service.py
@@ -9,7 +9,22 @@ _test = TestCase()
 
 
 @pytest.mark.delivery
-def test_calculate_delivery_in_ordinary_day(custon_holidays):
+def test_calculate_delivery_in_ordinary_day_with_default_calendar(default_holidays):
+    service = EstimativeDeliveryService(default_holidays)
+    result = service.estimate_delivery(date(2024, 5, 10), 5, 8)
+    _expcted_result = [
+        date(2024, 5, 16),
+        date(2024, 5, 17),
+        date(2024, 5, 20),
+        date(2024, 5, 21),
+    ]
+    _test.assertEqual(len(result), len(_expcted_result))
+    for item in result:
+        _test.assertIn(item, _expcted_result)
+
+
+@pytest.mark.delivery
+def test_calculate_delivery_in_ordinary_day_with_custon_calendar(custon_holidays):
     service = EstimativeDeliveryService(custon_holidays)
     result = service.estimate_delivery(date(2024, 5, 10), 5, 8)
     _expcted_result = [
@@ -24,22 +39,7 @@ def test_calculate_delivery_in_ordinary_day(custon_holidays):
 
 
 @pytest.mark.delivery
-def test_calculate_delivery_in_special_holidays(custon_holidays):
-    service = EstimativeDeliveryService(custon_holidays)
-    result = service.estimate_delivery(date(2024, 6, 20), 5, 8)
-    _expcted_result = [
-        date(2024, 6, 28),
-        date(2024, 7, 1),
-        date(2024, 7, 2),
-        date(2024, 7, 3),
-    ]
-    _test.assertEqual(len(result), len(_expcted_result))
-    for item in result:
-        _test.assertIn(item, _expcted_result)
-
-
-@pytest.mark.delivery
-def test_calculate_delivery_in_special_holidays_but_using_default_calendar(
+def test_calculate_delivery_in_special_holidays_with_default_calendar(
     default_holidays,
 ):
     service = EstimativeDeliveryService(default_holidays)
@@ -49,6 +49,21 @@ def test_calculate_delivery_in_special_holidays_but_using_default_calendar(
         date(2024, 6, 27),
         date(2024, 6, 28),
         date(2024, 7, 1),
+    ]
+    _test.assertEqual(len(result), len(_expcted_result))
+    for item in result:
+        _test.assertIn(item, _expcted_result)
+
+
+@pytest.mark.delivery
+def test_calculate_delivery_in_special_holidays_with_custon_calendar(custon_holidays):
+    service = EstimativeDeliveryService(custon_holidays)
+    result = service.estimate_delivery(date(2024, 6, 20), 5, 8)
+    _expcted_result = [
+        date(2024, 6, 28),
+        date(2024, 7, 1),
+        date(2024, 7, 2),
+        date(2024, 7, 3),
     ]
     _test.assertEqual(len(result), len(_expcted_result))
     for item in result:
